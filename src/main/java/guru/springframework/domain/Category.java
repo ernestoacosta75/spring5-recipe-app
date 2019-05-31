f3d7fdf611f958e194c30123f4cea6c6e9fe510f
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-05-31 23:28:44 +0200

    Added ManyToMany bidirectional relationship between Recipe and Category. The owner is Recipe.

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,46 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Simple Java Bean domain object representing a Category.
+ *
+ * @author Rodriguez Acosta Ernesto Antonio
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
